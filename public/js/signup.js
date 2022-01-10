8b7f544b8b5f6dadae41054a96e456875857b092
steven daddario <daddarios@dupage.edu>
2022-01-10 03:00:30 -0600

    requirements met

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,13 @@
 const form = document.querySelector('form'); 
+const successDiv = document.querySelector('#success-div');
 form.addEventListener('submit', async (evt)=>{
     evt.preventDefault(); 
 
-    const firstname = form.querySelector('input[name="firstname"]').value; 
-    const lastname = form.querySelector('input[name="lastname"]').value; 
+    const firstName = form.querySelector('input[name="firstName"]').value; 
+    const lastName = form.querySelector('input[name="lastName"]').value; 
     const email = form.querySelector('input[name="email"]').value; 
-    const body = JSON.stringify({firstname, lastname, email});
-
-    const results = await fetch('/users', {headers: {'Content-Type': 'application/json'}, body, method: 'POST'})
+    const body = JSON.stringify({firstName, lastName, email});
+    const data = await fetch('/users', {headers: {'Content-Type': 'application/json'}, body, method: 'POST'})
         .then(resp => {
             if(!resp.ok){
                 throw new Error("Error: Status", resp.status);
@@ -19,8 +19,14 @@ form.addEventListener('submit', async (evt)=>{
         })
         .catch(err => console.error(err));
 
-    console.log(results);
-    form.querySelector('input[name="firstname"]').value = ''
-    form.querySelector('input[name="lastname"]').value = ''; 
+    form.querySelector('input[name="firstName"]').value = ''
+    form.querySelector('input[name="lastName"]').value = ''; 
     form.querySelector('input[name="email"]').value  = ''; 
+
+    if(data.state){
+        successDiv.innerText = 'Account Successfully Created!';
+    }
+    else{
+        successDiv.innerText = '';
+    }
 });
